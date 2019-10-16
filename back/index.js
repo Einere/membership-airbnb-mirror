350bcc79a5354700bedd5c967e7c4a4bc1ea0479
Einenre <kjwsx23@gmail.com>
2019-10-16 16:51:57 +0900

    feat(cors): add whitelist, cors
    cors문제 해결을 위해 cors모듈과 whitelist를 추가함.
    추가로 static폴더인 public을 추가함.
    
    Signed-off-by: Einenre <kjwsx23@gmail.com>

--- back/index.js
+++ back/index.js
@@ -1,11 +1,28 @@
-import {GraphQLServer} from 'graphql-yoga';
+import {express, GraphQLServer} from 'graphql-yoga';
 import {resolvers} from './graphql/resolvers';
-
+// cors
+import {whitelist} from './cors/whitelist';
+import cors from 'cors';
 
 const server = new GraphQLServer({
     typeDefs: "./graphql/schema.graphql",
-    resolvers
+    resolvers,
+    options: {
+        static: 'public'
+    }
 });
+
+// cors
+server.express.use(cors({
+    origin: function (origin, callback) {
+        callback(whitelist.indexOf(origin) > -1 || !origin ? null : new Error('Not allowed by CORS'), true);
+        // 만약 서버 - 서버 api로 사용하고 싶으면 아래와 같이 코딩한다
+        // 참고 : https://expressjs.com/en/resources/middleware/cors.html
+        // callback(whitelist.indexOf(origin) > -1 || !origin ? null : new Error('Not allowed by CORS'), true);
+    },
+    methods: "GET, POST, PATCH, DELETE",
+    credentials: true,
+}));
 
 server.start(() => {
     console.log('graphQL server is running!');
