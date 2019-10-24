c9c810ec6b7fa58d8c76d928443152f8857aeb26
Einenre <kjwsx23@gmail.com>
2019-10-24 11:36:14 +0900

    fix(card list): remove useQuery code
    `RoomCardList`컴포넌트는 부모인 `Home`컴포넌트로 부터 렌더링 할 배열만 받는 역할을 수행함.
    따라서 직접 쿼리를 보내는 코드를 제거함.
    
    Signed-off-by: Einenre <kjwsx23@gmail.com>

--- front/src/components/RoomCardList.js
+++ front/src/components/RoomCardList.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import {useQuery} from "@apollo/react-hooks";
-import queries from "../graphql/queries";
 import RoomCard from '../components/RoomCard';
 import style from '../stylesheet/RoomCardList.module.css';
 
 function RoomCardList(props) {
-    const {loading, error, data} = useQuery(queries.getRooms);
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
-
-    // const result = data.getRooms.map(room => <li key={room.id}><img src={room.image} alt="thumbnail"/>{room.title}</li>);
-    const result = data.getRooms.map(room => <RoomCard key={room.id} id={room.id} image={room.image} title={room.title}
-                                                       options={room.options}/>);
+    const result = props.rooms.map(room => <RoomCard key={room.id} id={room.id} image={room.image} title={room.title}
+                                                     options={room.options}/>);
 
     return (
         <>
