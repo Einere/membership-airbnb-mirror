aff67eed043f10a8cee5c18e8ef6a89033256122
Einenre <kjwsx23@gmail.com>
2019-10-24 17:13:35 +0900

    fix(date filter): modify class to function
    클래스 문법으로 작성된 `DateFilter`컴포넌트를 함수 문법으로 수정함.
    
    Signed-off-by: Einenre <kjwsx23@gmail.com>

--- front/src/components/filter/DateFilter.js
+++ front/src/components/filter/DateFilter.js
@@ -1,40 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import moment from 'moment'
 import {DayPickerRangeController, isInclusivelyBeforeDay} from "react-dates";
 import 'react-dates/lib/css/_datepicker.css';
 import 'react-dates/initialize';
 
+function DateFilter(props) {
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+    const [focusedInput, setFocusedInput] = useState("startDate");
 
-export default class Calendar extends React.Component {
-    state = {
-        startDate: null,
-        endDate: null,
-
-        // 포커싱 될 기준을 start로 할지 end 로 할지 설정. 직접 바꿔보면 앎
-        // null 로 바꾸면 망함
-        focusedInput: "startDate",
-    };
-
-    render() {
-        return (
-            <DayPickerRangeController
-                startDate={this.state.startDate}
-                endDate={this.state.endDate}
-                // 모름
-                onDatesChange={({startDate, endDate}) =>
-                    this.setState({startDate, endDate})
-                }
-                // 모름
-                focusedInput={this.state.focusedInput}
-                // 모름
-                onFocusChange={focusedInput => {
-                    this.setState({focusedInput: focusedInput || "startDate"});
-                }}
-                // 지난 날짜 disable
-                isOutsideRange={day => isInclusivelyBeforeDay(day, moment().add(-1, 'days'))}
-                // 2달 씩 표시
-                numberOfMonths={2}
-            />
-        );
-    }
+    return (
+        <DayPickerRangeController
+            startDate={startDate}
+            endDate={endDate}
+            // 모름
+            onDatesChange={({startDate, endDate}) => {
+                setStartDate(startDate);
+                setEndDate(endDate);
+            }
+            }
+            // 모름
+            focusedInput={focusedInput}
+            // 모름
+            onFocusChange={focusedInput => {
+                setFocusedInput(focusedInput || "startDate");
+            }}
+            // 지난 날짜 disable
+            isOutsideRange={day => isInclusivelyBeforeDay(day, moment().add(-1, 'days'))}
+            // 2달 씩 표시
+            numberOfMonths={2}
+        />
+    );
 }
+
+export default DateFilter;
