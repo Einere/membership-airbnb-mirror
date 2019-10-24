41b5d19af80c0fd8be0e341ad5be433c4af507fe
Einenre <kjwsx23@gmail.com>
2019-10-24 11:14:09 +0900

    feat(header): add `Header.js`
    홈으로 가기 및 로그인 페이지를 쉽게 접근할 수 있도록 헤더를 추가함.
    
    Signed-off-by: Einenre <kjwsx23@gmail.com>

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './stylesheet/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import client from './apollo/apolloClient';
 import {ApolloProvider} from '@apollo/react-hooks';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './routes/Home';
 import User from './routes/User';
+import Header from "./components/Header";
 
 function App() {
     return (
         <ApolloProvider client={client}>
             <div className="App">
                 <BrowserRouter>
+                    <Header/>
                     <Switch>
                         <Route path="/user">
                             <User/>
