63e2b1ef1528ee1509c4aff7ca06811d0a9993ca
Einenre <kjwsx23@gmail.com>
2019-10-24 16:11:47 +0900

    feat(filter): add `PriceFilter` component to `FilterBar` component
    구현한 `PriceFilter`컴포넌트를 `FilterBar`컴포넌트에 추가함.
    
    Signed-off-by: Einenre <kjwsx23@gmail.com>

--- front/src/components/FilterBar.js
+++ front/src/components/FilterBar.js
@@ -4,10 +4,11 @@ import style from '../stylesheet/FilterBar.module.css';
 import {FilterContext} from "./context/FilterContext";
 import {QueryContext} from "./context/QueryContext";
 import {dateFilterReducer, initialDate} from '../utils/dateFilterReducer';
-import capacityFilterReducerWraper from "../utils/capacityFilterReducer";
-import {initialPrice, priceFilterReducer} from "../utils/priceFilterReducer";
+import capacityFilterReducerWrapper from "../utils/capacityFilterReducer";
+import priceFilterReducerWrapper from "../utils/priceFilterReducer";
 import DateFilter from "./filter/DateFilter";
 import CapacityFilter from "./filter/CapacityFilter";
+import PriceFilter from "./filter/PriceFilter";
 
 
 function FilterBar() {
@@ -15,8 +16,9 @@ function FilterBar() {
     const {state, dispatch} = useContext(QueryContext);
     // 필터 별 리듀서를 사용한다
     const [dateFilterState, dateFilterDispatch] = useReducer(dateFilterReducer, initialDate);
-    const {initialCapacity, capacityFilterReducer} = capacityFilterReducerWraper(dispatch);
+    const {initialCapacity, capacityFilterReducer} = capacityFilterReducerWrapper(dispatch);
     const [capacityFilterState, capacityFilterDispatch] = useReducer(capacityFilterReducer, initialCapacity);
+    const {initialPrice, priceFilterReducer} = priceFilterReducerWrapper(dispatch);
     const [priceFilterState, priceFilterDispatch] = useReducer(priceFilterReducer, initialPrice);
 
     return (
@@ -28,7 +30,7 @@ function FilterBar() {
                 <FilterButton filtertype={"capacity"}><CapacityFilter/></FilterButton>
             </FilterContext.Provider>
             <FilterContext.Provider value={{state: priceFilterState, dispatch: priceFilterDispatch}}>
-                <FilterButton filtertype={"price"}/>
+                <FilterButton filtertype={"price"}><PriceFilter/></FilterButton>
             </FilterContext.Provider>
         </nav>
     );
