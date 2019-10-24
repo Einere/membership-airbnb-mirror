a2c1c2b02f2492e3ddd0527ba7f5c3efbf1f10f1
Einenre <kjwsx23@gmail.com>
2019-10-24 17:50:23 +0900

    fix(date filter): add dispatch to FilterContext
    날짜가 변경이 될 때 마다 `FilterContext`에 `dispatch`하도록 수정함.
    
    Signed-off-by: Einenre <kjwsx23@gmail.com>

--- front/src/components/filter/DateFilter.js
+++ front/src/components/filter/DateFilter.js
@@ -1,10 +1,13 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import moment from 'moment'
 import {DayPickerRangeController, isInclusivelyBeforeDay} from "react-dates";
 import 'react-dates/lib/css/_datepicker.css';
 import 'react-dates/initialize';
+import {FilterContext} from "../context/FilterContext";
 
 function DateFilter(props) {
+    const {state, dispatch} = useContext(FilterContext);
+
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
     const [focusedInput, setFocusedInput] = useState("startDate");
@@ -17,6 +20,7 @@ function DateFilter(props) {
             onDatesChange={({startDate, endDate}) => {
                 setStartDate(startDate);
                 setEndDate(endDate);
+                dispatch({type: 'date', payload: {checkIn: startDate, checkOut: endDate}});
             }
             }
             // 모름
