b23b0f8ec8e1333456a83f22278f8cc263081f4b
Einenre <kjwsx23@gmail.com>
2019-10-24 10:55:39 +0900

    feat(filter): add some filters
    Modal컴포넌트 내부에 들어갈 필터 컴포넌트들을 추가함.
    
    Signed-off-by: Einenre <kjwsx23@gmail.com>

---
+++ front/src/components/CapacityFilter.js
@@ -0,0 +1,52 @@
+import React, {useContext} from 'react';
+import {FilterContext} from "./FilterContext";
+import style from '../stylesheet/CapacityFilter.module.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMinus, faPlus} from '@fortawesome/free-solid-svg-icons'
+
+function CapacityFilter() {
+    const {state, dispatch} = useContext(FilterContext);
+
+    function dispatchAdult(gap) {
+        return () => dispatch({type: 'adult', payload: gap});
+    }
+
+    function dispatchTeenager(gap) {
+        return () => dispatch({type: 'teenager', payload: gap});
+    }
+
+    function dispatchToddler(gap) {
+        return () => dispatch({type: 'toddler', payload: gap});
+    }
+
+    return (
+        <div style={style}>
+            <section className={style.itemContainer}>
+                <span>adult</span>
+                <div className={style.controllerContainer}>
+                    <FontAwesomeIcon icon={faPlus} size={'lg'} onClick={dispatchAdult(1)}/>
+                    {state.adult}
+                    <FontAwesomeIcon icon={faMinus} size={'lg'} onClick={dispatchAdult(-1)}/>
+                </div>
+            </section>
+            <section className={style.itemContainer}>
+                <span>teenager</span>
+                <div className={style.controllerContainer}>
+                    <FontAwesomeIcon icon={faPlus} size={'lg'} onClick={dispatchTeenager(1)}/>
+                    {state.teenager}
+                    <FontAwesomeIcon icon={faMinus} size={'lg'} onClick={dispatchTeenager(-1)}/>
+                </div>
+            </section>
+            <section className={style.itemContainer}>
+                <span>toddler</span>
+                <div className={style.controllerContainer}>
+                    <FontAwesomeIcon icon={faPlus} size={'lg'} onClick={dispatchToddler(1)}/>
+                    {state.toddler}
+                    <FontAwesomeIcon icon={faMinus} size={'lg'} onClick={dispatchToddler(-1)}/>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default CapacityFilter;
