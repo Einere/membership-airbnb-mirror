79082c7c21ab8cc487aa9fc9c58a3e8bbc11298d
Einenre <kjwsx23@gmail.com>
2019-10-24 11:12:29 +0900

    feat(query context): add QueryContext.Provider to `Home` component
    쿼리 결과를 공유하기 위한 QueryContext를 사용할 수 있도록 하기 위해 Provider를 추가함.
    
    Signed-off-by: Einenre <kjwsx23@gmail.com>

--- front/src/routes/Home.js
+++ front/src/routes/Home.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import {Link} from 'react-router-dom';
+import {useQuery} from "@apollo/react-hooks";
+import queries from "../graphql/queries";
+import {QueryContext} from '../components/QueryContext';
+import {initialQuery, queryReducer} from "../utils/queryReducer";
 import FilterBar from "../components/FilterBar";
 import RoomCardList from '../components/RoomCardList'
 
 function Home() {
+    const [queryState, queryDispatch] = useReducer(queryReducer, initialQuery);
+
+    const {loading, error, data} = useQuery(queries.getRooms);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
     return (
         <>
             <h1>Home</h1>
             <Link to="/">Home</Link>
             <Link to="/user/1">User 1</Link>
-            <FilterBar/>
-            <RoomCardList/>
+            <QueryContext.Provider value={{queryState, queryDispatch}}>
+                <FilterBar/>
+                <RoomCardList rooms={queryState.length ? queryState : data.getRooms}/>
+            </QueryContext.Provider>
+
         </>
     );
 }
