abf65f5cbde1e162b1b58ef7840820d5214ede83
Einenre <kjwsx23@gmail.com>
2019-10-25 15:36:47 +0900

    fix(front): modify client uri
    다시 로컬호스트로 변경
    
    Signed-off-by: Einenre <kjwsx23@gmail.com>

--- front/src/apollo/apolloClient.js
+++ front/src/apollo/apolloClient.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-    uri: 'http://45.119.146.248:4000/gql',
+    uri: 'http://localhost:4000/gql',
 });
 
 export default client;
