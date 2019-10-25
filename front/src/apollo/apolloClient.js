ae06322187a5f52cf203740ec8b42b1268cde04e
Einenre <kjwsx23@gmail.com>
2019-10-25 15:23:49 +0900

    fix(front): modify client uri
    배포하기위해, 공인 아이피로 임시 변경
    
    Signed-off-by: Einenre <kjwsx23@gmail.com>

--- front/src/apollo/apolloClient.js
+++ front/src/apollo/apolloClient.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/gql',
+    uri: 'http://45.119.146.248:4000/gql',
 });
 
 export default client;
